20d9d2fdc22a26e337a0548a54670be784d13401
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-02-12 19:21:22 -0600

    Merge issues

--- src/new/table3d.h
+++ src/new/table3d.h
@@ -12,41 +12,6 @@ This file is used for everything related to maps/tables including their definiti
 #define TABLE_SHIFT_FACTOR  8
 #define TABLE_SHIFT_POWER   (1UL<<TABLE_SHIFT_FACTOR)
 
-/*
-The 2D table can contain either 8-bit (int8_t) or 16-bit (int) values
-The valueSize variable should be set to either 8 or 16 to indicate this BEFORE the table is used
-*/
-struct table2D {
-  //Used 5414 RAM with original version
-  int8_t valueSize;
-  int8_t axisSize;
-  int8_t xSize;
-
-  void *values;
-  void *axisX;
-
-  //int16_t *values16;
-  //int16_t *axisX16;
-
-  //Store the last X and Y coordinates in the table. This is used to make the next check faster
-  int16_t lastXMax;
-  int16_t lastXMin;
-
-  //Store the last input and output for caching
-  int16_t lastInput;
-  int16_t lastOutput;
-  int8_t cacheTime; //Tracks when the last cache value was set so it can expire after x seconds. A timeout is required to pickup when a tuning value is changed, otherwise the old cached value will continue to be returned as the X value isn't changing. 
-};
-
-int16_t table2D_getAxisValue(struct table2D*, int8_t);
-int16_t table2D_getRawValue(struct table2D*, int8_t);
-
-template <class _Ty>
-struct coord2D {
-  _Ty x = 0;
-  _Ty y = 0;
-};
-
 struct table3D {
 
   table3D(int8_t size) : axisSize(size) {}
--- src/new/table3d.hpp
+++ src/new/table3d.hpp
@@ -1,86 +1,37 @@
 #include <Arduino.h>
-
-<<<<<<< HEAD:src/new/table3d.hpp
-//The shift amount used for the 3D table calculations
-#define TABLE_SHIFT_FACTOR  8
-#define TABLE_SHIFT_POWER   (1UL<<TABLE_SHIFT_FACTOR)
-
-  //This function pulls a value from a 3D table given a target for X and Y coordinates.
-  //It performs a 2D linear interpolation as descibred in: www.megamanual.com/v22manual/ve_tuner.pdf
-  int8_t get3DTableValue(int16_t Y_in, int16_t X_in, const int8_t *values, int8_t yDim, int8_t xDim, int16_t *ppAxisX, int16_t *pAsisY)
-{
-    int16_t X = X_in;
-    int16_t Y = Y_in;
-=======
 #include "table3d.h"
 
 
 //This function pulls a value from a 3D table given a target for X and Y coordinates.
 //It performs a 2D linear interpolation as descibred in: www.megamanual.com/v22manual/ve_tuner.pdf
-int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
+int get3DTableValue(table3D *fromTable, int Y_in, int X_in)
   {
     int X = X_in;
     int Y = Y_in;
->>>>>>> 840ba6c (Back to basics):src/new/table.hpp
 
     int tableResult = 0;
     //Loop through the X axis bins for the min/max pair
-    //Note: For the X axis specifically, rather than looping from tablepAxisX[0] up to tablepAxisX[max], we start at tablepAxisX[Max] and go down.
+    //Note: For the X axis specifically, rather than looping from tableAxisX[0] up to tableAxisX[max], we start at tableAxisX[Max] and go down.
     //      This is because the important tables (fuel and injection) will have the highest RPM at the top of the X axis, so starting there will mean the best case occurs when the RPM is highest (And hence the CPU is needed most)
-<<<<<<< HEAD:src/new/table3d.hpp
-    int16_t xMinValue = pAxisX[0];
-    int16_t xMaxValue = pAxisX[xDim-1];
-    int8_t xMin = 0;
-    int8_t xMax = 0;
-=======
-    const int16_t* pXAxis = fromTable->getXAxis();
+    const int16_t *pXAxis = fromTable->getXAxis();
     int xMinValue = pXAxis[0];
     int xMaxValue = pXAxis[fromTable->axisSize-1];
     byte xMin = 0;
     byte xMax = 0;
->>>>>>> 840ba6c (Back to basics):src/new/table.hpp
 
     //If the requested X value is greater/small than the maximum/minimum bin, reset X to be that value
     if(X > xMaxValue) { X = xMaxValue; }
     if(X < xMinValue) { X = xMinValue; }
 
     //0th check is whether the same X and Y values are being sent as last time. If they are, this not only prevents a lookup of the axis, but prevents the interpolation calcs being performed
-    if( (X_in == lastXInput) && (Y_in == lastYInput) && (cacheIsValid == true))
+    if( (X_in == fromTable->lastXInput) && (Y_in == fromTable->lastYInput) && (fromTable->cacheIsValid == true))
     {
-      return lastOutput;
+      return fromTable->lastOutput;
     }
 
     //Commence the lookups on the X and Y axis
 
     //1st check is whether we're still in the same X bin as last time
-<<<<<<< HEAD:src/new/table3d.hpp
-    if ( (X <= pAxisX[lastXMax]) && (X > pAxisX[lastXMin]) )
-    {
-      xMaxValue = pAxisX[lastXMax];
-      xMinValue = pAxisX[lastXMin];
-      xMax = lastXMax;
-      xMin = lastXMin;
-    }
-    //2nd check is whether we're in the next RPM bin (To the right)
-    else if ( ((lastXMax + 1) < xDim ) && (X <= pAxisX[lastXMax +1 ]) && (X > pAxisX[lastXMin + 1]) ) //First make sure we're not already at the last X bin
-    {
-      xMax = lastXMax + 1;
-      lastXMax = xMax;
-      xMin = lastXMin + 1;
-      lastXMin = xMin;
-      xMaxValue = pAxisX[lastXMax];
-      xMinValue = pAxisX[lastXMin];
-    }
-    //3rd check is to look at the previous bin (to the left)
-    else if ( (lastXMin > 0 ) && (X <= pAxisX[lastXMax - 1]) && (X > pAxisX[lastXMin - 1]) ) //First make sure we're not already at the first X bin
-    {
-      xMax = lastXMax - 1;
-      lastXMax = xMax;
-      xMin = lastXMin - 1;
-      lastXMin = xMin;
-      xMaxValue = pAxisX[lastXMax];
-      xMinValue = pAxisX[lastXMin];
-=======
     if ( (X <= pXAxis[fromTable->lastXMax]) && (X > pXAxis[fromTable->lastXMin]) )
     {
       xMaxValue = pXAxis[fromTable->lastXMax];
@@ -107,12 +58,11 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
       fromTable->lastXMin = xMin;
       xMaxValue = pXAxis[fromTable->lastXMax];
       xMinValue = pXAxis[fromTable->lastXMin];
->>>>>>> 840ba6c (Back to basics):src/new/table.hpp
     }
     else
     //If it's not caught by one of the above scenarios, give up and just run the loop
     {
-      for (int8_t x = xDim-1; x >= 0; x--)
+      for (int8_t x = fromTable->axisSize-1; x >= 0; x--)
       {
         //Checks the case where the X value is exactly what was requested
         if ( (X == pXAxis[x]) || (x == 0) )
@@ -120,9 +70,9 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
           xMaxValue = pXAxis[x];
           xMinValue = pXAxis[x];
           xMax = x;
-          lastXMax = xMax;
+          fromTable->lastXMax = xMax;
           xMin = x;
-          lastXMin = xMin;
+          fromTable->lastXMin = xMin;
           break;
         }
         //Normal case
@@ -131,61 +81,26 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
           xMaxValue = pXAxis[x];
           xMinValue = pXAxis[x-1];
           xMax = x;
-          lastXMax = xMax;
+          fromTable->lastXMax = xMax;
           xMin = x-1;
-          lastXMin = xMin;
+          fromTable->lastXMin = xMin;
           break;
         }
       }
     }
 
     //Loop through the Y axis bins for the min/max pair
-<<<<<<< HEAD:src/new/table3d.hpp
-    int16_t yMaxValue = pAxisY[0];
-    int16_t yMinValue = pAxisY[yDim-1];
-    int8_t yMin = 0;
-    int8_t yMax = 0;
-=======
-    const int16_t* pYAxis = fromTable->getYAxis();
+    const int16_t *pYAxis = fromTable->getYAxis();
     int yMaxValue = pYAxis[0];
     int yMinValue = pYAxis[fromTable->axisSize-1];
     byte yMin = 0;
     byte yMax = 0;
->>>>>>> 840ba6c (Back to basics):src/new/table.hpp
 
     //If the requested Y value is greater/small than the maximum/minimum bin, reset Y to be that value
     if(Y > yMaxValue) { Y = yMaxValue; }
     if(Y < yMinValue) { Y = yMinValue; }
 
     //1st check is whether we're still in the same Y bin as last time
-<<<<<<< HEAD:src/new/table3d.hpp
-    if ( (Y >= pAxisY[lastYMax]) && (Y < pAxisY[lastYMin]) )
-    {
-      yMaxValue = pAxisY[lastYMax];
-      yMinValue = pAxisY[lastYMin];
-      yMax = lastYMax;
-      yMin = lastYMin;
-    }
-    //2nd check is whether we're in the next MAP/TPS bin (Next one up)
-    else if ( (lastYMin > 0 ) && (Y <= pAxisY[lastYMin - 1 ]) && (Y > pAxisY[lastYMax - 1]) ) //First make sure we're not already at the top Y bin
-    {
-      yMax = lastYMax - 1;
-      lastYMax = yMax;
-      yMin = lastYMin - 1;
-      lastYMin = yMin;
-      yMaxValue = pAxisY[lastYMax];
-      yMinValue = pAxisY[lastYMin];
-    }
-    //3rd check is to look at the previous bin (Next one down)
-    else if ( ((lastYMax + 1) < yDim) && (Y <= pAxisY[lastYMin + 1]) && (Y > pAxisY[lastYMax + 1]) ) //First make sure we're not already at the bottom Y bin
-    {
-      yMax = lastYMax + 1;
-      lastYMax = yMax;
-      yMin = lastYMin + 1;
-      lastYMin = yMin;
-      yMaxValue = pAxisY[lastYMax];
-      yMinValue = pAxisY[lastYMin];
-=======
     if ( (Y >= pYAxis[fromTable->lastYMax]) && (Y < pYAxis[fromTable->lastYMin]) )
     {
       yMaxValue = pYAxis[fromTable->lastYMax];
@@ -212,13 +127,12 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
       fromTable->lastYMin = yMin;
       yMaxValue = pYAxis[fromTable->lastYMax];
       yMinValue = pYAxis[fromTable->lastYMin];
->>>>>>> 840ba6c (Back to basics):src/new/table.hpp
     }
     else
     //If it's not caught by one of the above scenarios, give up and just run the loop
     {
 
-      for (int8_t y = yDim-1; y >= 0; y--)
+      for (int8_t y = fromTable->axisSize-1; y >= 0; y--)
       {
         //Checks the case where the Y value is exactly what was requested
         if ( (Y == pYAxis[y]) || (y==0) )
@@ -226,9 +140,9 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
           yMaxValue = pYAxis[y];
           yMinValue = pYAxis[y];
           yMax = y;
-          lastYMax = yMax;
+          fromTable->lastYMax = yMax;
           yMin = y;
-          lastYMin = yMin;
+          fromTable->lastYMin = yMin;
           break;
         }
         //Normal case
@@ -237,9 +151,9 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
           yMaxValue = pYAxis[y];
           yMinValue = pYAxis[y-1];
           yMax = y;
-          lastYMax = yMax;
+          fromTable->lastYMax = yMax;
           yMin = y-1;
-          lastYMin = yMin;
+          fromTable->lastYMin = yMin;
           break;
         }
       }
@@ -258,18 +172,11 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
               C          D
 
     */
-<<<<<<< HEAD:src/new/table3d.hpp
-    int A = values[yMin][xMin];
-    int B = values[yMin][xMax];
-    int C = values[yMax][xMin];
-    int D = values[yMax][xMax];
-=======
-    const int8_t* pValues = fromTable->getValues() ;
+    const int8_t *pValues = fromTable->getValues();
     int A = pValues[yMin * fromTable->axisSize + xMin];
     int B = pValues[yMin * fromTable->axisSize + xMax];
     int C = pValues[yMax * fromTable->axisSize + xMin];
     int D = pValues[yMax * fromTable->axisSize + xMax];
->>>>>>> 840ba6c (Back to basics):src/new/table.hpp
 
     //Check that all values aren't just the same (This regularly happens with things like the fuel trim maps)
     if( (A == B) && (A == C) && (A == D) ) { tableResult = A; }
@@ -306,11 +213,10 @@ int get3DTableValue(struct table3D *fromTable, int Y_in, int X_in)
     }
 
     //Update the tables cache data
-    lastXInput = X_in;
-    lastYInput = Y_in;
-    lastOutput = tableResult;
-    cacheIsValid = true;
+    fromTable->lastXInput = X_in;
+    fromTable->lastYInput = Y_in;
+    fromTable->lastOutput = tableResult;
+    fromTable->cacheIsValid = true;
 
     return tableResult;
-  }
-  
+}
